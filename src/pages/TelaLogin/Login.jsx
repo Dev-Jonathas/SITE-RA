0883ba23e91954cfbc888d63809f22327b61b359
Jonathas Xavier <Jonathasx677@gmail.com>
2024-11-06 21:32:47 -0300

    up

--- src/pages/TelaLogin/Login.jsx
+++ src/pages/TelaLogin/Login.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "../TelaLogin/Login.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const navigate = useNavigate();
 
  // Função de login para conectar ao backend
- const login = async (email, senha) => {
+ const handleLogin = async (email, senha) => {
   try {
-    const response = await fetch("https://locallhost:8080/login", {
+    const response = await fetch("http://localhost:8080/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -21,44 +20,13 @@ const Login = () => {
     if (!response.ok) {
       return { success: false, message: "Credenciais inválidas" };
     }
-
-    const data = await response.json();
-    const { token, type } = data;
-
-    // Armazenando o token e o tipo de usuário
-    localStorage.setItem("token", token);
-    localStorage.setItem("userType", type);
-
-    return { success: true, type };
   } catch (error) {
     console.error("Erro na autenticação:", error);
     return { success: false, message: "Erro no servidor" };
   }
 };
 
-// Função para tratar o envio do formulário de login
-const handleLogin = async (e) => {
-  e.preventDefault();
 
-  if (!email || !senha) {
-    alert("Por favor, preencha todos os campos.");
-    return;
-  }
-
-  const { success, type } = await login(email, senha);
-
-  // Redirecionamento condicional
-  if (success) {
-    if (type === "common") {
-      navigate("/user");
-    } else if (type === "ies") {
-      navigate("/IES");
-    }
-  } else {
-    alert("Falha no login. Verifique suas credenciais.");
-  }
-
-};
   return (
     <div className="login-page">
       <div className="cadastro-container">
